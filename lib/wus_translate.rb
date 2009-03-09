bc0eee38194758d0b97ec364684ede536825e422
patricktulskiem <patricktulskie@gmail.com>
2009-03-08 22:55:04 -0400

    Began working on the translation library.

---
+++ lib/wus_translate.rb
@@ -0,0 +1,20 @@
+require "rtranslate"
+
+class WusTranslate
+  
+  def initialize
+    # Are translation services available?
+    
+  end  
+    
+  def translate_text(text, destination, source="en")
+    
+    result = Translate.t(text, Language::ENGLISH, Language::CHINESE_SIMPLIFIED)
+  end
+  
+  def translation_available?
+    # try and do a test translation with the available gems
+    
+  end  
+  
+end
