e7ad43b743855363be1aa3a269f8afea77399c98
patricktulskiem <patricktulskie@gmail.com>
2009-03-15 15:26:44 -0400

    Just working on translation.

--- lib/wus_translate.rb
+++ lib/wus_translate.rb
@@ -8,13 +8,13 @@ class WusTranslate
   end  
     
   def translate_text(text, destination, source="en")
-    
+    # Use whatever libraries are available
     result = Translate.t(text, Language::ENGLISH, Language::CHINESE_SIMPLIFIED)
   end
   
   def translation_available?
     # try and do a test translation with the available gems
     
-  end  
+  end
   
 end
