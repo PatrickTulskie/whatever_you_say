dd2d14c8670f6c35d9e745ca0ae041685d191599
patricktulskiem <patricktulskie@gmail.com>
2009-05-07 02:01:30 -0400

    doing some more neat analytics stuff.

--- lib/wus_analytics.rb
+++ lib/wus_analytics.rb
@@ -7,14 +7,25 @@ class WusAnalytics
   def interesting_statistics
     data = {}
     data['users_who_speak_same_language'] = users_who_speak_same_language.length
+    data['users_who_speak_other_languages'] = users_who_speak_other_languages.length
+    data['most_common_language'] = most_common_language
     return data
   end
-  
-  private
   
   def users_who_speak_same_language
     language = @user.language
     language.users
-  end    
+  end
+  
+  def users_who_speak_other_languages
+    users = User.all - @user.language.users
+  end
+  
+  def most_common_language
+    counts = {}
+    langs = Language.find(:all, :include => :users)
+    langs.each{ |language| counts[language.name] = language.users.length }
+    return counts.sort{ |a,b| a.last <=> b.last }.last.first
+  end
     
 end
