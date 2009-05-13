56d4b38e9f8285fae07dd38a7eb96eb4f7d08bd2
patricktulskiem <patricktulskie@gmail.com>
2009-05-13 13:55:19 -0400

    Fixed the profile language user indexing migration.

--- db/migrate/20090513173820_add_index_to_profiles.rb
+++ db/migrate/20090513173820_add_index_to_profiles.rb
@@ -1,9 +1,11 @@
 class AddIndexToProfiles < ActiveRecord::Migration
   def self.up
-    add_index :profiles, :language_id
+    add_index :profiles, [:language_id, :user_id]
+    add_index :profiles, :user_id
   end
 
   def self.down
-    remove_index :profiles, :language_id
+    remove_index :profiles, [:language_id, :user_id]
+    remove_index :profiles, :user_id
   end
 end
